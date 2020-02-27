41ad01b00729d786db456a6d7b162c5e6690e36d
Shuheng Liu @ Ali-Shanghai-2 <wish1104@outlook.com>
2020-02-28 00:23:20 +0800

    switch to port 3000 on alicloud

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.get('/about', (req, res) => {
 app.use('/ideas', ideas);
 app.use('/users', users);
 
-const port = 5000;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
